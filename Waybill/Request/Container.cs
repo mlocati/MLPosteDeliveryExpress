ea6376e8082253893ed0924c87f6338efb9b7ec5
Michele Locati <michele@locati.it>
2023-02-15 19:18:57 +0100

    Add sandbox cost center code

--- Waybill/Request/Container.cs
+++ Waybill/Request/Container.cs
@@ -8,6 +8,8 @@ namespace MLPosteDeliveryExpress.Waybill.Request
 {
     public class Container
     {
+        public const string SANDBOX_COST_CENTER_CODE = "CDC-00070964";
+
         /// <summary>
         /// Codice del centro di costo.
         /// </summary>
