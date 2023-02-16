7c9dd7f17e1bc9aeb4048b6cd79989ccb68334e1
Michele Locati <michele@locati.it>
2023-02-16 15:20:10 +0100

    Remove Account.DisplayName, add DisplayName attrs to Account

--- Account.cs
+++ Account.cs
@@ -1,20 +1,22 @@
-﻿namespace MLPosteDeliveryExpress
+﻿using System.ComponentModel;
+
+namespace MLPosteDeliveryExpress
 {
     public class Account
     {
         public const string SANDBOX_COST_CENTER_CODE = "CDC-00070964";
 
-        public string DisplayName { get; set; }
-
+        [DisplayName("Client ID")]
         public string ClientID { get; set; }
 
+        [DisplayName("Client Secret")]
         public string ClientSecret { get; set; }
 
+        [DisplayName("Cost Center Code")]
         public string CostCenterCode { get; set; }
 
-        public Account(string displayName, string clientID, string clientSecret, string costCenterCode)
+        public Account(string clientID, string clientSecret, string costCenterCode)
         {
-            this.DisplayName = displayName;
             this.ClientID = clientID;
             this.ClientSecret = clientSecret;
             this.CostCenterCode = costCenterCode;
