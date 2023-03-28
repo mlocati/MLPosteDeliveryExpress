15467ecac1a3f6fa7e9aecab36000286f735af0d
Michele Locati <michele@locati.it>
2023-03-28 14:07:54 +0200

    Update sandbox account

--- Library/SandboxAccount.cs
+++ Library/SandboxAccount.cs
@@ -5,10 +5,10 @@ namespace MLPosteDeliveryExpress
     public sealed class SandboxAccount : IAccount
     {
         [DisplayName("Client ID")]
-        public string ClientID { get => "c7cd7028-0f4c-4623-99b2-a0c088947be5"; }
+        public string ClientID { get => "611c9cb0-deff-44b8-8337-b5fe2b3297fc"; }
 
         [DisplayName("Client Secret")]
-        public string ClientSecret { get => "7yh_PwK3Q9X.d_T4-U.YG_S04gyEBcY.36"; }
+        public string ClientSecret { get => "aSn8Q~5LbgHtdwqr~DYJ14z5UBTowLBqMPMonbG."; }
 
         [DisplayName("Cost Center Code")]
         public string CostCenterCode { get => "CDC-00070964"; }
