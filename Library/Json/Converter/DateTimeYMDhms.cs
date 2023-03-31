4cb0772e1272814bf21f7607b80ff8e3e36be458
Michele Locati <michele@locati.it>
2023-03-31 09:36:21 +0200

    Fix tracking time zone

--- Library/Json/Converter/DateTimeYMDhms.cs
+++ Library/Json/Converter/DateTimeYMDhms.cs
@@ -7,17 +7,37 @@ namespace MLPosteDeliveryExpress.Json.Converter
 {
     internal class DateTimeYMDhms : JsonConverter<DateTime>
     {
+        private static readonly Lazy<TimeZoneInfo> SerializedTimezone = new(() => DateTimeYMDhms.GetSourceTimezone());
+
         private const string FORMAT = @"yyyy-MM-dd HH\:mm\:ss";
 
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            return DateTime.ParseExact(reader.GetString() ?? "", FORMAT, CultureInfo.InvariantCulture).ToLocalTime();
+            var jsonDateTime = reader.GetString() ?? "";
+            var serializedDateTime = DateTime.ParseExact(jsonDateTime, FORMAT, CultureInfo.InvariantCulture);
+            return TimeZoneInfo.ConvertTimeToUtc(serializedDateTime, DateTimeYMDhms.SerializedTimezone.Value).ToLocalTime();
         }
 
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
         {
-            var str = value.ToString(FORMAT, CultureInfo.InvariantCulture);
+            var serializedDateTime = TimeZoneInfo.ConvertTimeFromUtc(value.ToUniversalTime(), DateTimeYMDhms.SerializedTimezone.Value);
+            var str = serializedDateTime.ToString(FORMAT, CultureInfo.InvariantCulture);
             writer.WriteStringValue(str);
+        }
+
+        private static TimeZoneInfo GetSourceTimezone()
+        {
+            try
+            {
+                return TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+            }
+            catch { }
+            try
+            {
+                return TimeZoneInfo.FindSystemTimeZoneById("Europe/Rome");
+            }
+            catch { }
+            return TimeZoneInfo.Local;
         }
     }
 }
