3d23f557f12734bed31889ff651dc04b439c461d
Michele Locati <michele@locati.it>
2025-02-13 10:43:24 +0100

    Add StatusDetector

---
+++ Test/StatusDetectorTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MLPosteDeliveryExpress.Service;
+
+namespace Test
+{
+    [TestClass]
+    public class StatusDetectorTest
+    {
+        [TestMethod]
+        public void DetectionWorks()
+        {
+            try
+            {
+                StatusDetector.IsWebServiceAvailableAsync(false).ConfigureAwait(false).GetAwaiter().GetResult();
+            }
+            catch (Exception x)
+            {
+                Assert.Fail("StatusDetector shouldn't throw an exception, but we had " + x.ToString());
+            }
+            try
+            {
+                StatusDetector.IsWebServiceAvailableAsync(true).ConfigureAwait(false).GetAwaiter().GetResult();
+            }
+            catch (Exception x)
+            {
+                Assert.Fail("StatusDetector shouldn't throw an exception, but we had " + x.ToString());
+            }
+        }
+    }
+}
