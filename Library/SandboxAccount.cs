2e1205b10e8a741aa25757e810b72c123d0ae2f4
Michele Locati <michele@locati.it>
2025-04-24 10:54:17 +0200

    Update Sandbox credentials

--- Library/SandboxAccount.cs
+++ Library/SandboxAccount.cs
@@ -5,10 +5,10 @@ namespace MLPosteDeliveryExpress
     public sealed class SandboxAccount : ISandboxAccount
     {
         [DisplayName("Client ID")]
-        public string ClientID { get => "611c9cb0-deff-44b8-8337-b5fe2b3297fc"; }
+        public string ClientID { get => "980a6661-cd8c-43c4-b6a1-d8f2a5593994"; }
 
         [DisplayName("Client Secret")]
-        public string ClientSecret { get => "aSn8Q~5LbgHtdwqr~DYJ14z5UBTowLBqMPMonbG."; }
+        public string ClientSecret { get => "3TW8Q~zWspadrQUNUKhFiu4eCoogittm~ZFK8bv6"; }
 
         [DisplayName("Cost Center Code")]
         public string CostCenterCode { get => "CDC-00070964"; }
